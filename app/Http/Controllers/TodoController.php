82fb0b06531375f30b55a461967bff6006bfd678
Lintangms14 <merayusukmalintang@gmail.com>
2025-04-25 20:26:27 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,21 +7,31 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    public function index()
-    {
-        //$todos = Todo::all();
-        $todos = Todo::where('user_id', Auth::id())->get();
-        dd($todos);
-        return view('todo.index');
+    public function index() {
+        // $todos = Todo::all();
+        $todos = Todo::where('user_id', Auth::id())->orderBy('created_at', 'desc')->get();
+        // dd($todos);
+        return view("todo.index", compact("todos"));
     }
 
-    public function create()
-    {
-        return view('todo.create');
+    public function create() {
+        return view("todo.create");
     }
 
-    public function edit()
+    public function edit() {
+        return view("todo.edit");
+    }
+
+    public function store(Request $request)
     {
-        return view('todo.edit');
+        $request->validate([
+            'title'=>'required|string|max:255',
+        ]);
+
+        $todo =Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => Auth::id(),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully');
     }
 }
